401da022fda68516633df12bf53e3453f343968a
Micah Elliott <mde@MicahElliott.com>
2017-03-28 16:31:38 -0700

    smerge hydra

--- personal/hydra.el
+++ personal/hydra.el
@@ -10,7 +10,7 @@
   ("l" text-scale-decrease "out"))
 
 (defhydra hydra-buffer-menu (:color pink
-                             :hint nil)
+                                    :hint nil)
   "
 ^Mark^             ^Unmark^           ^Actions^          ^Search
 ^^^^^^^^-----------------------------------------------------------------
@@ -78,8 +78,8 @@ _~_: modified
 (global-set-key
  "\C-o"
  (defhydra my/switch-to-buffer (:exit t
-                                :body-pre (setq my/last-buffers
-                                                (my/name-of-buffers 4)))
+                                      :body-pre (setq my/last-buffers
+                                                      (my/name-of-buffers 4)))
    "
 _o_ther buffers: %s(my/number-names my/last-buffers)
 
@@ -196,6 +196,40 @@ _SPC_ cancel   _o_nly this     _d_elete
    ("Z" winner-redo)
    ("SPC" nil)
    ))
+
+
+;; https://www.snip2code.com/Snippet/1507385/Hydra-for-smerge/
+(global-set-key
+ (kbd "C-M-S-s")
+ (defhydra hydra-smerge
+   (:color red :hint nil
+           :pre (smerge-mode 1))
+   "
+^Move^ ^Keep^ ^Diff^ ^Pair^
+------------------------------------------------------
+_n_ext _b_ase _R_efine _<_: base-mine
+_p_rev _m_ine _E_diff _=_: mine-other
+^ ^ _o_ther _C_ombine _>_: base-other
+^ ^ _a_ll _r_esolve
+_q_uit _RET_: current
+"
+   ("RET" smerge-keep-current)
+   ("C" smerge-combine-with-next)
+   ("E" smerge-ediff)
+   ("R" smerge-refine)
+   ("a" smerge-keep-all)
+   ("b" smerge-keep-base)
+   ("m" smerge-keep-mine)
+   ("n" smerge-next)
+   ("o" smerge-keep-other)
+   ("p" smerge-prev)
+   ("r" smerge-resolve)
+   ("<" smerge-diff-base-mine)
+   ("=" smerge-diff-mine-other)
+   (">" smerge-diff-base-other)
+   ("q" nil :color blue)))
+
+
 
 
 (provide 'hydra)
